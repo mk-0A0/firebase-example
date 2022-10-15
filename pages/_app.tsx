6101c1959cc9f9db33c3814d6d9c3b515acaac42
momoka <moowfaw@gmail.com>
2022-10-15 23:13:11 +0900

    firebaseConfigのexport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
 import {ChakraProvider} from "@chakra-ui/react";
+import {app} from "../lib/firebaseConfig"
+
+app()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
