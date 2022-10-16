0633f799187508ffb77bc3c806314f2f9b9f30c8
momoka <moowfaw@gmail.com>
2022-10-17 00:26:14 +0900

    ログアウト機能実装

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,11 +1,28 @@
-import {Button, Container, Heading, Spacer} from "@chakra-ui/react";
+import {Button, Container, Heading, Spacer, useToast} from "@chakra-ui/react";
+import {FirebaseError} from "@firebase/app";
+import {getAuth, signOut} from "@firebase/auth";
 
 export const mypage = () => {
-  return(
+  const toast = useToast()
+  const onLogout = async () => {
+    console.log('click')
+    try {
+      const auth = getAuth()
+      await signOut(auth)
+      toast({status: "success", title: 'ログアウトしました'})
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.log(e)
+        toast({status: "error", title: 'ログアウトに失敗しました'})
+      }
+    }
+  }
+
+  return (
     <Container py={10}>
       <Heading>マイページ</Heading>
-      <Spacer h={10} />
-      <Button>ログアウト</Button>
+      <Spacer h={10}/>
+      <Button onClick={() => onLogout()}>ログアウト</Button>
     </Container>
   )
 }
