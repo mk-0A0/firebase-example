caa980207ae51609eb13010833c2d5c5ba66dddc
momoka <moowfaw@gmail.com>
2022-10-16 17:28:39 +0900

    ログイン機能実装

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,6 +1,27 @@
-import {Button, Container, FormControl, FormLabel, Heading, Input, Spacer, Stack} from "@chakra-ui/react"
+import {Button, Container, FormControl, FormLabel, Heading, Input, Spacer, Stack, useToast} from "@chakra-ui/react"
+import {useState} from "react";
+import {FirebaseError} from "@firebase/app";
+import {getAuth, signInWithEmailAndPassword} from "@firebase/auth";
 
 export const signin = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const toast = useToast()
+
+  const onSignin = async () => {
+    console.log(email, password)
+    try {
+      const auth = getAuth()
+      await signInWithEmailAndPassword(auth, email, password)
+      toast({status: "success", title: 'ログインしました'})
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.log(e)
+        toast({status: "error", title: 'ログインに失敗しました'})
+      }
+    }
+  }
+
   return (
     <Container py={10}>
       <Heading>ログイン</Heading>
@@ -8,13 +29,13 @@ export const signin = () => {
       <Stack>
         <FormControl>
           <FormLabel>メールアドレス</FormLabel>
-          <Input type={'email'}/>
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} type={'email'}/>
         </FormControl>
         <FormControl>
           <FormLabel>パスワード</FormLabel>
-          <Input type={'password'}/>
+          <Input value={password} onChange={(e) => setPassword(e.target.value)} type={'password'}/>
         </FormControl>
-        <Button>ログイン</Button>
+        <Button onClick={() => onSignin()}>ログイン</Button>
       </Stack>
     </Container>
   )
