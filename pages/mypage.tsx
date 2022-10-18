f46361e2a5b80923a9fcb1cf8c489147a91222f3
momoka <moowfaw@gmail.com>
2022-10-19 02:07:09 +0900

    ログアウト後のページ遷移をpathpidaに修正

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,14 +1,19 @@
 import {Button, Container, Heading, Spacer, useToast} from "@chakra-ui/react";
 import {FirebaseError} from "@firebase/app";
 import {getAuth, signOut} from "@firebase/auth";
+import {useRouter} from "next/router";
+import {pagesPath} from "../lib/$path";
 
 export const mypage = () => {
   const toast = useToast()
+  const {push} = useRouter()
+
   const onLogout = async () => {
     console.log('click')
     try {
       const auth = getAuth()
       await signOut(auth)
+      await push(pagesPath.$url())
       toast({status: "success", title: 'ログアウトしました'})
     } catch (e) {
       if (e instanceof FirebaseError) {
